8ae9f927da133448df7cf4971f28d9dcdb6ed8ee
perens <sanderperens@gmail.com>
2021-01-19 20:35:54 +0200

    comment print

--- Solvers/AnnealingSolver.py
+++ Solvers/AnnealingSolver.py
@@ -176,8 +176,8 @@ class AnnealingSolver:
                 
                 # found the solution
                 if energy_new == self.optimal_energy:
-                    print('solution with temp', temp)
-                    print('remaining epochs', epoch)
+                    # print('solution with temp', temp)
+                    # print('remaining epochs', epoch)
                     return next_neigbhour.flatten()
                 
                 delta_energy = energy_current - energy_new
